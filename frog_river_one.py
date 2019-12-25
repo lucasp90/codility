9acd96a8f647087dbb093157222170dbad49b85c
lucasp90 <lucas.lerp@gmail.com>
2019-12-25 15:43:48 -0300

    Frog river one (27)

--- frog_river_one.py
+++ frog_river_one.py
@@ -1,6 +1,3 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
 def solution(X, A):
     # write your code in Python 3.6
     positions = dict.fromkeys(range(1,X+1), 0)
@@ -9,13 +6,18 @@ def solution(X, A):
     waiting_for_missing_leaf = False
     for time, position in enumerate(A):
         if position <= X:
-            positions[position] =+ 1
-            covered_positions = covered_positions+1 if positions[position] == 1 else covered_positions
-        if waiting_for_missing_leaf:
-            additional_time += time
+            positions[position] += 1
+            if positions[position] == 1:
+                covered_positions += 1
+                if waiting_for_missing_leaf:
+                    additional_time += time
         if position == X:
             if covered_positions == X:
                 return time
             else:
                 waiting_for_missing_leaf = True
                 additional_time = time
+        else:
+            if covered_positions == X:
+                return additional_time
+    return -1
